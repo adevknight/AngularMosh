886c46b6ef4c0c1cf93626306739b37f39d59398
ADevKnight <adevknight@gmail.com>
2018-01-31 01:24:11 +0700

    106 Rendering to Templates

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,10 +7,11 @@ import { Http } from '@angular/http';
   styles: []
 })
 export class PostsComponent implements OnInit {
+  posts: any[];
 
   constructor(http: Http) { 
     http.get('https://jsonplaceholder.typicode.com/posts').subscribe(res => {
-      console.log(res.json());
+      this.posts = res.json();
     });
   }
 
