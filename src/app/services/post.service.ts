4da4699a13e1c3dddded863217b3c6b003f0bdd1
ADevKnight <adevknight@gmail.com>
2018-02-02 11:25:14 +0700

    118 FIXING TypeError: Observable_1.Observable.throw is not a function (on 116 Throwing App Specific Errors (NotFoundError or 404))

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class PostService {
