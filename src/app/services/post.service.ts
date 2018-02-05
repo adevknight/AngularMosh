b6b8a3d964dda917a5c8fb86c4809564ae26d7de
ADevKnight <adevknight@gmail.com>
2018-02-05 10:58:11 +0700

    120 Reusable Error Handling Method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -14,30 +14,30 @@ export class PostService {
   constructor(private http: Http) { }
 
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url).catch(this.handleErrors);
   }
 
   createPost(x) {
-    return this.http.post(this.url, JSON.stringify(x)).catch((err: Response) => {
-      if (err.status === 400) {
-        return Observable.throw(new BadInputError(err.json()));
-      } else {
-        return Observable.throw(new AppError(err.json()));
-      }
-    });
+    return this.http.post(this.url, JSON.stringify(x)).catch(this.handleErrors);
   }
 
   updatePost(x) {
-    return this.http.put(this.url + '/' + x.id, JSON.stringify(x));
+    return this.http.put(this.url + '/' + x.id, JSON.stringify(x)).catch(this.handleErrors);
   }
 
   deletePost(x) {
-    return this.http.delete(this.url + '/' + x.id).catch((err: Response) => {
-      if (err.status === 404) {
-        return Observable.throw(new NotFoundError(err.json()));
-      } else {
-        return Observable.throw(new AppError(err.json()));
-      }
-    });
+    return this.http.delete(this.url + '/' + x.id).catch(this.handleErrors);
+  }
+
+  private handleErrors(err: Response) {
+    if (err.status === 400) {
+      return Observable.throw(new BadInputError(err.json()));
+    }
+
+    if (err.status === 404) {
+      return Observable.throw(new NotFoundError(err.json()));
+    }
+
+    return Observable.throw(new AppError(err.json()));
   }
 }
