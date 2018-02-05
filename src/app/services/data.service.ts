c04740b561e82f9a9aa0486ac42292f89181e1ef
ADevKnight <adevknight@gmail.com>
2018-02-05 12:52:25 +0700

    121 Reusable Data Service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,42 @@
+import { AppError } from './../common/errors/app-error';
+import { NotFoundError } from './../common/errors/not-found-error';
+import { Observable } from 'rxjs/Observable';
+import { BadInputError } from './../common/errors/bad-input-error';
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private url, private http: Http) { }
 
+  getPosts() {
+    return this.http.get(this.url).catch(this.handleErrors);
+  }
+
+  createPost(x) {
+    return this.http.post(this.url, JSON.stringify(x)).catch(this.handleErrors);
+  }
+
+  updatePost(x) {
+    return this.http.put(this.url + '/' + x.id, JSON.stringify(x)).catch(this.handleErrors);
+  }
+
+  deletePost(x) {
+    return this.http.delete(this.url + '/' + x.id).catch(this.handleErrors);
+  }
+
+  private handleErrors(err: Response) {
+    if (err.status === 400) {
+      return Observable.throw(new BadInputError(err.json()));
+    }
+
+    if (err.status === 404) {
+      return Observable.throw(new NotFoundError(err.json()));
+    }
+
+    return Observable.throw(new AppError(err.json()));
+  }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,43 +1,10 @@
-import { BadInputError } from './../common/errors/bad-input-error';
-import { NotFoundError } from './../common/errors/not-found-error';
-import { AppError } from './../common/errors/app-error';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+import { DataService } from './data.service';
 
 @Injectable()
-export class PostService {
-  private url = 'https://abcjsonplaceholder.typicode.com/posts';
-
-  constructor(private http: Http) { }
-
-  getPosts() {
-    return this.http.get(this.url).catch(this.handleErrors);
-  }
-
-  createPost(x) {
-    return this.http.post(this.url, JSON.stringify(x)).catch(this.handleErrors);
-  }
-
-  updatePost(x) {
-    return this.http.put(this.url + '/' + x.id, JSON.stringify(x)).catch(this.handleErrors);
-  }
-
-  deletePost(x) {
-    return this.http.delete(this.url + '/' + x.id).catch(this.handleErrors);
-  }
-
-  private handleErrors(err: Response) {
-    if (err.status === 400) {
-      return Observable.throw(new BadInputError(err.json()));
-    }
-
-    if (err.status === 404) {
-      return Observable.throw(new NotFoundError(err.json()));
-    }
-
-    return Observable.throw(new AppError(err.json()));
+export class PostService extends DataService {
+  constructor(http: Http) {
+    super('https://jsonplaceholder.typicode.com/posts', http);
   }
 }
