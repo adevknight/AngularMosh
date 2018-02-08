f9cf41cafd6d3ba8cd1403a1d9cc398a6ddc092c
ADevKnight <adevknight@gmail.com>
2018-02-08 08:17:35 +0700

    130 Using routerLinkActive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: []
+  styles: [`
+    .bg {
+      background-color: #eaeaea;
+    }
+  `]
 })
 export class NavbarComponent implements OnInit {
 
