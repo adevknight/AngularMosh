96a56ecaa391d7559932c2e13fb21707722805a2
ADevKnight <adevknight@gmail.com>
2018-02-09 13:09:27 +0700

    040 Property Binding

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CoursesComponent implements OnInit {
+  title = 'This is the TITLE LOREM IPSUM DOLOR';
+  imgUrlA = 'http://placehold.it/600/92c952';
+  imgUrlB = 'http://placehold.it/600/771796';
 
   constructor() { }
 
