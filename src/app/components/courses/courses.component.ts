359f55d7627a0022d4df6487e0a0dc1e6d4ab02d
ADevKnight <adevknight@gmail.com>
2018-02-09 13:38:03 +0700

    043 Class Binding

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
   title = 'This is the TITLE LOREM IPSUM DOLOR';
-  imgUrlA = 'http://placehold.it/600/92c952';
-  imgUrlB = 'http://placehold.it/600/771796';
+  x = true;
 
   constructor() { }
 
