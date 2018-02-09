6814fbc6432e55014e35ff8edee2e1401ec332eb
ADevKnight <adevknight@gmail.com>
2018-02-09 11:13:34 +0700

    136 Using .switchMap to Clean Up Nested .subscribe() - Introduction

--- src/app/components/github-followers/github-followers.component.ts
+++ src/app/components/github-followers/github-followers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-github-followers',
@@ -41,20 +42,40 @@ export class GithubFollowersComponent implements OnInit {
   constructor(private currentRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    // If you want to call somethingService whenever route's parameters or query parameters
+    // are changed then use .switchMap instead of nesting .somethingService inside .subscribe.
+    // Nesting .somethingService inside .subscribe will cause
+    // the nesting of .subscribe (of .somethingService) inside .subscribe
+
+    // Remember to use .switchMap() not .map()
+
     Observable.combineLatest([
       this.currentRoute.paramMap,
       this.currentRoute.queryParamMap
-    ]).subscribe(combined => {
-      // combined[0] will always refer to streams of data obtained from paramMap Observable
-      // combined[1] will always refer to streams of data obtained from queryParamMap Observable
+    ]).switchMap(combined => {
+      // do whatever you wanted to do with value of route's parameters and query parameters.
 
-      console.log('username: ' + combined[0].get('username'));
-
-      this.page = +combined[1].get('page');
-      console.log('page: ' + this.page);
-      this.sortBy = combined[1].get('sortBy');
-      console.log('sortBy: ' + this.sortBy);
+      return this.somethingService.someMethod();
+    }).subscribe(val => {
+      // do whatever you wanted to do with val returned from .somethingService.someMethod().
     });
+
+    /*
+      Let say you have following statements
+      obsA.switchMap(x => {
+        return obsB;
+      }).subscribe(y => {
+        console.log(y);
+      });
+
+      It means that wheneven obsA emit new value, we emit obsB value from very beginning again.
+      E.g:
+      obsA is an Observable of <button> click events, and
+      obsB is an Observable of incremental number values, i.e 1,2,3,4,5,6,so on
+
+      If we implement .switchMap() like above the stream of data become something like:
+      [Click],1,2,3,[Click],1,2,[Click],1,2,3,4,5,6,7,8,[Click],1,2,[Click],1,so on
+    */
   }
 
 }
