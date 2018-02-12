182c43e069b14874d5d5ace9003f6cdaa0b1eedd
ADevKnight <adevknight@gmail.com>
2018-02-12 19:06:41 +0700

    066 [ngSwitch], *ngSwitchCase, *ngSwitchDefault

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [1, 2, 3, 4, 5];
+  viewMode = 'something';
+  changeViewMode(x) {
+    this.viewMode = x;
+  }
 }
