836b766cf09cb04af349f52ad6c781ecdcd31555
ADevKnight <adevknight@gmail.com>
2018-02-13 16:51:03 +0700

    074 @HostListener('eventName')

--- src/app/directives/input-format.directive.ts
+++ src/app/directives/input-format.directive.ts
@@ -1,9 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
+  @HostListener('focus') onFocus() {
+    console.log('On Focus...');
+  }
+  @HostListener('blur') onBlur() {
+    console.log('On Blur...');
+  }
 
   constructor() { }
 
