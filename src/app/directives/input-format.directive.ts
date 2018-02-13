bcd3a7edcd1f36bfc42d5889df306c84dd44abed
ADevKnight <adevknight@gmail.com>
2018-02-13 17:58:36 +0700

    074 Custom Directive with Input Property

--- src/app/directives/input-format.directive.ts
+++ src/app/directives/input-format.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
+  @Input('format') format: string;
+
   @HostListener('blur') onBlur() {
     const value: string = this.x.nativeElement.value;
-    this.x.nativeElement.value = value.toUpperCase();
+    if (this.format === 'uppercase') {
+      this.x.nativeElement.value = value.toUpperCase();
+    } else if (this.format === 'lowercase') {
+      this.x.nativeElement.value = value.toLowerCase();
+    }
     console.log('On Blur...');
   }
 
