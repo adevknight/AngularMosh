5f576aea37ce075d1566c02d23ab33b251a700d5
ADevKnight <adevknight@gmail.com>
2018-02-13 16:34:07 +0700

    074 BLANK-ing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  task = {
-    title: 'Amet quis quis consequat ea occaecat do.',
-    assignee: {
-      name: null
-    }
-  };
+
 }
