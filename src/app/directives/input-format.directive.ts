d3e912adfe5ec554fcf6e77614af4b7932efb397
ADevKnight <adevknight@gmail.com>
2018-02-13 17:01:38 +0700

    074 Implementing toUpperCase Directive

--- src/app/directives/input-format.directive.ts
+++ src/app/directives/input-format.directive.ts
@@ -1,16 +1,15 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-  @HostListener('focus') onFocus() {
-    console.log('On Focus...');
-  }
   @HostListener('blur') onBlur() {
+    const value: string = this.x.nativeElement.value;
+    this.x.nativeElement.value = value.toUpperCase();
     console.log('On Blur...');
   }
 
-  constructor() { }
+  constructor(private x: ElementRef) { }
 
 }
