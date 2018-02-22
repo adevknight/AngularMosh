119a0da6674c0f56bd8b1e6640df225b3ec9b5e3
ADevKnight <adevknight@gmail.com>
2018-02-22 10:27:29 +0700

    004 Linking <form> in Template with ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
@@ -12,7 +12,8 @@ import { FormComponent } from './components/form/form.component';
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
